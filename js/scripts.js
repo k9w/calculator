64ef0e7af4ab7b59824d987069d280baa736329e
Kevin Williams <kevin@k9w.org>
2022-01-17 18:33:46 -0800

    Flesh out the ADD function.

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,7 @@ function add(number1, numbe2) {
   return number1 + number2;
 }
 
-alert(add(10, 5));
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+
+alert(add(number1, number2));
