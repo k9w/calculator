81c75604d564eadb12cc669d2767bbcde3d133ad
Kevin Williams <kevin@k9w.org>
2022-02-20 10:19:53 -0800

    Consolidate output into a single alert popup: one reault per line.

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,9 @@ function divide(number1, number2) {
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-alert(number1 + ' plus ' + number2 + ' equals ' + add(number1, number2));
-alert(number1 + ' minus ' + number2 + ' equals ' + subtract(number1, number2));
-alert(number1 + ' times ' + number2 + ' equals ' + multiply(number1, number2));
-alert(number1 + ' divided by ' + number2 + ' equals ' + divide(number1, number2));
+const addResult = number1 + ' plus ' + number2 + ' equals ' + add(number1, number2);
+const subtractResult = number1 + ' minus ' + number2 + ' equals ' + subtract(number1, number2);
+const multiplyResult = number1 + ' times ' + number2 + ' equals ' + multiply(number1, number2);
+const divideResult = number1 + ' divided by ' + number2 + ' equals ' + divide(number1, number2);
+
+alert(addResult + '\n' + subtractResult + '\n' + multiplyResult + '\n' + divideResult);
