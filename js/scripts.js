1a1770331a4f12ca1d70ce7fdb655c1a27973457
Kevin Williams <kevin@k9w.org>
2022-02-20 10:07:35 -0800

    Now calculate all four operations.

--- js/scripts.js
+++ js/scripts.js
@@ -17,4 +17,7 @@ function divide(number1, number2) {
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-alert(divide(number1, number2));
+alert(number1 + ' plus ' + number2 + ' equals ' + add(number1, number2));
+alert(number1 + ' minus ' + number2 + ' equals ' + subtract(number1, number2));
+alert(number1 + ' times ' + number2 + ' equals ' + multiply(number1, number2));
+alert(number1 + ' divided by ' + number2 + ' equals ' + divide(number1, number2));
